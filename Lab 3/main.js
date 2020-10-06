86738f3b8b4ac1de316fc9ff276f89772f1fcd49
Harsh Kavdikar (Student) <hkavdika@sundevils.asu.edu>
2020-10-06 00:10:32 -0700

    Updated code with error  handling

--- Lab 3/main.js
+++ Lab 3/main.js
@@ -4,7 +4,7 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     cookieParser = require("cookie-parser"),
     session = require('express-session');
-    app = express();
+app = express();
 
 
 // Set it to set the tempalate engine
@@ -19,8 +19,8 @@ app.use(cookieParser());
 
 // Initiate session cookies options
 app.use(session({
-	secret: 'magic',
-	resave: true,
+    secret: 'magic',
+    resave: true,
     saveUninitialized: true,
     name: 'sessionid'
 }));
@@ -30,27 +30,31 @@ var questions = {}
 
 var userInfo = {}
 
-fs.readFile("survey.json", "utf-8", function(error, data) {
+fs.readFile("survey.json", "utf-8", function (error, data) {
     questions = JSON.parse(data).questions;
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.redirect("/landing")
 })
 
-app.get("/landing", function(req, res) {
-    res.render("landing.ejs")
+app.get("/landing", function (req, res) {
+    res.render("landing.ejs", {
+        username: req.cookies.username == undefined ? "" : req.cookies.username
+    })
 })
 
-app.post("/landing", function(req, res) {
-
+app.post("/landing", function (req, res) {
     let username = req.body.username
     res.cookie("username", username);
-    if (username in userInfo == false){
+
+    if (req.cookies.preference == undefined)
+        res.cookie("preference", "horizontal");
+
+    if (username in userInfo == false) {
         userInfo[username] = {
             currentPage: 0,
-            preference: "horizontal",
-            selectedChoices: new Array(questions.length).fill(-1)
+            selectedChoices: {}
         }
     }
     else {
@@ -58,54 +62,93 @@ app.post("/landing", function(req, res) {
     }
 
     if (req.body.action == "survey")
-        res.redirect(307, "/survey")
+        res.redirect("/survey")
     else
         res.redirect("/match")
 })
 
-app.post("/survey", function(req, res) {
-    let username = req.cookies.username
-    let preference = userInfo[username].preference
-    let page = userInfo[username].currentPage;
-    let selectedChoices = userInfo[username].selectedChoices
-    if (req.body.choice != undefined)
-        userInfo[username].selectedChoices[page - 1] = req.body.choice
-
-    userInfo[username].currentPage++;
-
-    if (page == questions.length) {
-        res.clearCookie("sessionid")
-        req.session.destroy();
+app.post("/survey", function (req, res) {
+    if (req.cookies.sessionid == undefined) {
         res.redirect("/")
         return
     }
 
+    let username = req.cookies.username
+
+    // Set answer for the current question
+    let page = userInfo[username].currentPage;
+    let questionid = questions[page].id
+    userInfo[username].selectedChoices[questionid] = req.body.choice === undefined ? -1 : req.body.choice
+
+    if (req.body.action == "prev") {
+        userInfo[username].currentPage--;
+        res.redirect("/survey");
+        return;
+    }
+
+    if (page == questions.length - 1) {
+        res.clearCookie("sessionid")
+        req.session.destroy();
+        res.render("finish.ejs")
+        // setTimeout(function() {
+        //     res.render("landing.ejs")
+        // },
+        // 10000);
+        return
+    }
+
+    // Get question info for next question
+    userInfo[username].currentPage++;
+    page = userInfo[username].currentPage;
+    questionid = questions[page].id
+
+    let preference = req.cookies.preference
+
+    let selectedChoice = userInfo[username].selectedChoices[questionid]
+
     res.render("survey.ejs", {
-        question : questions[page].question,
-        options : questions[page].choices,
-        page : page + 1,
-        selectedChoice : selectedChoices[page],
-        username : username,
-        preference : preference,
-        prevFlag : page == 0 ? false : true
+        question: questions[page].question,
+        options: questions[page].choices,
+        page: page + 1,
+        selectedChoice: selectedChoice === undefined ? -1 : selectedChoice,
+        username: username,
+        preference: preference,
+        prevFlag: page == 0 ? false : true
     })
 })
 
-app.get("/match", function(req, res){
+app.get("/survey", function (req, res) {
+
+    let username = req.cookies.username
+    let preference = req.cookies.preference
+    let page = userInfo[username].currentPage;
+    let questionid = questions[page].id
+    let selectedChoice = userInfo[username].selectedChoices[questionid]
+
+    res.render("survey.ejs", {
+        question: questions[page].question,
+        options: questions[page].choices,
+        page: page + 1,
+        selectedChoice: selectedChoice == undefined ? -1 : selectedChoice,
+        username: username,
+        preference: preference,
+        prevFlag: page == 0 ? false : true
+    })
+})
+
+app.get("/match", function (req, res) {
     res.send("match")
 })
 
-app.get("/preference", function(req, res){
-    let username = req.cookies.username
+app.get("/preference", function (req, res) {
     res.render("preference.ejs", {
-        preference: userInfo[username].preference
+        preference: req.cookies.preference
     })
 })
 
-app.post("/preference", function(req, res){
-    let username = req.cookies.username
-    userInfo[username].preference = req.body.preference
-    res.redirect(307, "/survey")
+app.post("/preference", function (req, res) {
+    res.cookie("preference", req.body.preference);
+    res.redirect("/survey");
 })
 
 app.listen(3000)
